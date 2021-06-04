54078352d0b5a63981f30741d0301e9cae337d3f
Simone Scigliuzzi <simonescigliuzzi@Simones-MBP.home>
2021-06-04 09:40:40 +0200

    Chiamate API su https

--- APP/src/environments/environment.ts
+++ APP/src/environments/environment.ts
@@ -9,13 +9,13 @@ export const environment = {
   APP_VERSION: '0.0.1',
 
   WEB_SITE_LOCAL: 'http://127.0.0.1:8000/',                 // localhost website
-  WEB_SITE: 'http://gestionale.mgnservice.it/',             // public website url
+  WEB_SITE: 'https://gestionale.mgnservice.it/',             // public website url
 
   API_TOKEN: '5be65b9c-2902-4490-9640-45f8c6ad360b',        // API token
   API_LOGGER_ENABLED: false,                                 // enabled/disable API calling debug on console
   API_USE_LOCAL: true,                                      // when not on device, if true, point on API_END_POINT_LOCAL
   API_END_POINT_LOCAL: 'http://127.0.0.1:8000/api/app',     // localhost endpoint
-  API_END_POINT: 'http://gestionale.mgnservice.it/api/app', // public endpoint
+  API_END_POINT: 'https://gestionale.mgnservice.it/api/app', // public endpoint
 
   LOCATION_TIMEOUT: 10,                                     // GPS location: request timout (in seconds)
   LOCATION_INERVAL: 15,                                     // interval (in seconds)
